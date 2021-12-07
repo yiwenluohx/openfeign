b144e82b7320309083e6a8786f2ddc097bef8b1b
luohx <Lhongxiao@lcchain.com>
2021-12-07 19:29:13 +0800

    添加restTemplate重试机制

--- src/main/java/com/study/openfeign/OpenFeignApplication.java
+++ src/main/java/com/study/openfeign/OpenFeignApplication.java
@@ -1,8 +1,13 @@
 package com.study.openfeign;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.retry.annotation.EnableRetry;
+import org.springframework.web.client.RestTemplate;
 
 /**
  * @Author: luohx
@@ -11,8 +16,29 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
  */
 @SpringBootApplication
 @EnableFeignClients
+/**
+ * 让重试机制生效
+ */
+@EnableRetry
 public class OpenFeignApplication {
+
+    /**
+     * 由于在controller中注入了RestTemplate，所以启动的时候需要实例化该类的一个实例
+     */
+    @Autowired
+    private RestTemplateBuilder builder;
+
     public static void main(String[] args) {
         SpringApplication.run(OpenFeignApplication.class, args);
+    }
+
+    /**
+     * 使用RestTemplateBuilder来实例化RestTemplate对象，spring默认已经注入了RestTemplateBuilder实例
+     *
+     * @return
+     */
+    @Bean
+    public RestTemplate restTemplate() {
+        return builder.build();
     }
 }
--- src/main/java/com/study/openfeign/controller/OrderController.java
+++ src/main/java/com/study/openfeign/controller/OrderController.java
@@ -2,9 +2,19 @@ package com.study.openfeign.controller;
 
 import com.study.openfeign.client.OrderClient;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.retry.annotation.Backoff;
+import org.springframework.retry.annotation.Retryable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestClientException;
+import org.springframework.web.client.RestTemplate;
+
+import javax.annotation.Resource;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 /**
  * @Author: luohx
@@ -18,10 +28,26 @@ public class OrderController {
     @Autowired
     private OrderClient orderClient;
 
+    @Resource
+    private RestTemplate restTemplate;
+
+    private static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+
     @GetMapping("/feignOkHttp")
-    public String feignOkHttp(){
+    public String feignOkHttp() {
         String result = orderClient.getOrderById("2");
         return result;
+    }
+
+    @GetMapping("/restTemplate")
+    @Retryable(value = RestClientException.class, maxAttempts = 3,
+            backoff = @Backoff(delay = 5000L,multiplier = 2))
+    public HttpStatus testRest() {
+        System.out.println("发起远程API请求:" + DATE_TIME_FORMATTER.format(LocalDateTime.now()));
+        String url = "http://jsonplaceholder.typicode.com/postss/1";
+        ResponseEntity<String> responseEntity
+                = restTemplate.getForEntity(url, String.class);
+        return responseEntity.getStatusCode(); // 获取响应码
     }
 
 }
